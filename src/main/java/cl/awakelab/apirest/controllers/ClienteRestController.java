4eecb04aefec09a759f0653ee9544adab1d42e93
Miguel Ramos <return.infox@gmail.com>
2023-06-20 10:56:25 -0400

    api rest

---
+++ src/main/java/cl/awakelab/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,49 @@
+package cl.awakelab.apirest.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.awakelab.apirest.models.entity.Cliente;
+import cl.awakelab.apirest.models.services.IClienteService;
+
+@RestController
+@RequestMapping("/api") 
+public class ClienteRestController {
+
+  @Autowired
+  private IClienteService clienteService;
+  
+  // localhost:8080/api/clientes
+  @GetMapping("/clientes")
+  @ResponseStatus(HttpStatus.OK)
+  public List<Cliente> index() {
+    return clienteService.findAll();
+  }
+  //localhost:8080/api/clientes/1
+  // Por id
+  @GetMapping("/clientes/{id}")
+  public Optional<Cliente> show(@PathVariable Long id) {
+    Optional<Cliente> optionalCliente = clienteService.findById(id);
+    
+    if(!optionalCliente.isPresent()) throw new ClienteNotFoundException("No existe un cliente con ese id: " +  id);
+    
+    return optionalCliente;
+  }
+  
+
+}
+
+class ClienteNotFoundException extends RuntimeException {
+ 
+  public ClienteNotFoundException(String message) {
+    super(message);
+  }
+}
